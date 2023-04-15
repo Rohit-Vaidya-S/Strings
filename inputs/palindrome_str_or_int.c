1fef44163b96eec5c5baa6c3124003aee677e5ed
Rohit-Vaidya-S <104826093+Rohit-Vaidya-S@users.noreply.github.com>
2023-04-15 09:21:46 +0530

    Update palindrome_str_or_int.c

--- inputs/palindrome_str_or_int.c
+++ inputs/palindrome_str_or_int.c
@@ -1,49 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main() {
-  int original, reversed_int = 0, remainder;
-  char str[100], reversed_str[100];
-
-  printf("Enter a string or integer: ");
-  scanf("%s", str);
-
-  // check if input is an integer
-  if (isdigit(str[0])) {
-    original = atoi(str);
-
-    // reversed integer is stored in reversed_int variable
-    int n = original;
-    while (n != 0) {
-        remainder = n % 10;
-        reversed_int = reversed_int * 10 + remainder;
-        n /= 10;
+int main(){
+    char *str1 = (char *)malloc(sizeof(char));
+    char *str2 = (char *)malloc(sizeof(char));
+    char *resultantStr = (char *)malloc(sizeof(char));
+    scanf("%[^\n]s",str1);
+    scanf(" %[^\n]s",str2);
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int k = 0;
+    for(int i = 0; i < len1; i++){
+        int found = 0;
+        for(int j = 0; j < len2; j++){
+            if(str1[i] == str2[j]){
+                found = 1;
+                break;
+            }
+        }
+        if(!found){
+            resultantStr[k++] = str1[i];
+        }
     }
-
-    // palindrome if original and reversed are equal
-    if (original == reversed_int)
-        printf("%d is a palindrome.", original);
-    else
-        printf("%d is not a palindrome.", original);
-    
-  } else {
-    int i, length;
-    length = strlen(str);
-
-    // reverse the string
-    for (i = 0; i < length; i++) {
-        reversed_str[length - 1 - i] = str[i];
+    for(int i = 0; i < len2; i++){
+        int found = 0;
+        for(int j = 0; j < len1; j++){
+            if(str2[i] == str1[j]){
+                found = 1;
+                break;
+            }
+        }
+        if(!found){
+            resultantStr[k++] = str2[i];
+        }
     }
-    reversed_str[length] = '\0'; // add null character at the end
-
-    // check if the original and reversed strings are equal
-    if (strcmp(str, reversed_str) == 0)
-        printf("%s is a palindrome.", str);
-    else
-        printf("%s is not a palindrome.", str);
-  }
-
-  return 0;
+    resultantStr[k] = '\0';
+    printf("%s", resultantStr);
+    free(str1);
+    free(str2);
+    free(resultantStr);
+  
+    return 0;
 }
