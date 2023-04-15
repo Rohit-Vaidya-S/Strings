f75fbe43422b3f58c51dc1fd095bb78fcaa7077c
Rohit-Vaidya-S <104826093+Rohit-Vaidya-S@users.noreply.github.com>
2023-04-15 22:31:14 +0530

    Update key_value.c

--- inputs/key_value.c
+++ inputs/key_value.c
@@ -13,16 +13,11 @@ int main() {
     printf("Enter a string of comma-separated values representing student grades: ");
     fgets(input, 1000, stdin);
 
-    // Remove the newline character from the input string
-    input[strcspn(input, "\n")] = '\0';
-
     // Parse the input string
     token = strtok(input, ",");
     while (token != NULL) {
         name = token;
-        printf("name: %s\n", name);
         grade_str = strtok(NULL, ",");
-        printf("grade_str: %s\n", grade_str);
         if (grade_str != NULL) {
             total += atoi(grade_str);
             count++;
