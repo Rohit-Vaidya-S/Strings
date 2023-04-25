f130af4c3b4b0c4d8ab075fe5ad016fd95562c8d
Rohit-Vaidya-S <104826093+Rohit-Vaidya-S@users.noreply.github.com>
2023-04-25 15:06:10 +0530

    Update Delete-Duplicate-Strings.c

--- Delete-Duplicate-Strings.c
+++ Delete-Duplicate-Strings.c
@@ -1,17 +1,18 @@
+// Deleting duplicate strings from an sotred data.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Deleting duplicate strings from an sotred data.
 
 int main() {
     char *storeData;
     int maxLen = 100; 
     storeData = (char *)malloc(maxLen * sizeof(char));
+    printf("Enter the strings with duplicate words: ");
     scanf("%[^\n]s", storeData);
     char *token = strtok(storeData, " "); 
     char *words[100]; 
     int i, j, count = 0;
+    printf("Strings without duplicate word: ");
     while (token != NULL) {
         for (i = 0; i < count; i++) {
             if (strcmp(words[i], token) == 0) {
